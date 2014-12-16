d8994251a2232ecf27a68ab509dd7585eaa174bd
shweetshourpowk <rmccrory1993@gmail.com>
2014-12-16 14:32:57 -0500

    yeah this thing went down.

--- js/main.js
+++ js/main.js
@@ -21,9 +21,61 @@ $(document).ready(function(){
             var song = songs[index];
             console.log(song);
 
-            $("#descArea").html("<div id='desc'>" + song.Pic + "<h2>" + song.Title + "</h2>" + "<p>" + song.Artist + " " + song.Album + " " + song.Date + " " + song.Genre + "</p>" + "<p>"+ song.Desc + "</p>" +  "</div>");
+            $("#descArea").html("<div id='desc'>" + song.Pic + "<h2>" + song.Title + "</h2>" + "<p>" + song.Artist + " " + song.Album + " " + song.Date + " " + song.Genre + "</p>" + "<p>"+ song.Desc + "</p>" + "</div>");
 
+            //$("#audio").html("<source src='" + song.Song + "' type='audio/wav'>");
+
+            var myAudio = document.querySelector("#audio");
+            myAudio.src = song.Song;
+            myAudio.addEventListener('canplay', function(event){
+                myAudio.play();
+            });
+            myAudio.load();
+
+
+
+            //var playing = false;
+            //
+            ////get dat sound
+            //var source = $('source');
+            //var musicProgress = document.getElementById('musicProgress');
+            //var soundStatus = document.getElementById('soundStatus');
+            //myAudio.addEventListener('ended', function(event) {
+            //    soundStatus.innerHTML = "Not playing";
+            //});
+            //
+            //var soundBtn = document.getElementById('index');
+            //$(this)('click', function(event) {
+            //    myAudio.play();
+            //    playing = true;
+            //});
+            //
+            ////waits until the audio is ready to play
+            //myAudio.addEventListener('canplay', function(event) {
+            //    //	myAudio.
+            //});
+            //
+            //setInterval(function() {
+            //    if(playing) {
+            //        musicProgress.value = (myAudio.currentTime / myAudio.duration) * 100;
+            //        console.log(myAudio.currentTime + "/" + myAudio.duration);
+            //    }
+            //}, 10);
+            //
+            //myAudio.addEventListener('ended', function(event) {
+            //    musicProgress.value = 100;
+            //    playing = false;
+            //});
+            //
+            //
+            //var changeSound = document.getElementById('changeSound');
+            //changeSound.addEventListener('click', function(event) {
+            //    myAudio.src= song.Song;
+            //    myAudio.load();
+            //})
         });
+
+
 
     })
 
